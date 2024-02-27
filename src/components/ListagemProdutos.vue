fe10942b4f5c9589f1e2f1a5224c2ab81a7127f0
AnaFidencio <fidenciosilvaanaclara@gmail.com>
2024-02-27 10:14:34 -0300

    Alteração no blocode script

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,23 +1,27 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
-
 const produtos = ref([]);
 
 onMounted(async () => {
   const response = await axios.get('https://fakestoreapi.com/products');
   produtos.value = response.data;
 });
+
+const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
 </script>
 
 <template>
-  <div>
-    <h1>Produtos</h1>
-    <div v-for="produto in produtos" :key="produto.id">
-      <h2>{{ produto.title }}</h2>
-      <p>{{ produto.description }}</p>
-      <p>{{ produto.price }}</p>
-      <img :src="produto.image" :alt="produto.title" />
+    <div>
+      <h1>Produtos</h1>
+      <div class="container">
+        <div class="card" v-for="produto in produtos" :key="produto.id">
+          <h1 class="card--title">{{ produto.title }}</h1>
+          <p>{{ produto.description }}</p>
+          <p>{{ formatPrice(produto.price) }}</p>
+          <img class="card--avatar" :src="produto.image" :alt="produto.title" />
+        </div>
+      </div>
     </div>
-  </div>
-</template>
+  </template>
+  
