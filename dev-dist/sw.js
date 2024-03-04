4272417c8126fc04e1343b917a17089f8151ee72
AnaFIdencio <fidenciosilvaanaclara@gmail.com>
2024-03-04 11:25:15 -0300

    consables

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a90hk6iabr"
+    "revision": "0.la4fe9ljbfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
